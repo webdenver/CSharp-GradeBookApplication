ad2bb62c9b50ba656f2072948336acb437f76ecd
webdenver <webdenver@github.com>
2020-05-31 22:57:11 +0500

    module 2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using GradeBook.GradeBooks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
