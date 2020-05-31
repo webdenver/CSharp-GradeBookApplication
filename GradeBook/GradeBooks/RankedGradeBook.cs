c10647c642b921fb7f1a858e2a1e50a4200a7e49
webdenver <webdenver@github.com>
2020-05-31 23:28:31 +0500

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.GradeBooks;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,24 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name): base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count<5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            int X = (int)Math.Round(Students.Count * 20.0 / 100,0);
+            var orderedGrades = Students.OrderBy(s => s.AverageGrade).Select(s => s.AverageGrade).Reverse();
+            var aGradeStudents = orderedGrades.Take(X);
+            var bGradeStudents = orderedGrades.Skip(X).Take(X);
+            var cGradeStudents = orderedGrades.Skip(X * 2).Take(X);
+            var dGradeStudents = orderedGrades.Skip(X * 3).Take(X);
+            if (averageGrade >= aGradeStudents.Min()) return 'A';
+            if (averageGrade >= bGradeStudents.Min()) return 'B';
+            if (averageGrade >= cGradeStudents.Min()) return 'C';
+            if (averageGrade >= dGradeStudents.Min()) return 'D';
+            return 'F';
         }
     }
 }
